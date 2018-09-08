f0b742d5c21928922c9f360e3bf0cdbf7eee92fe
EC2 Default User <sfrees@ramapo.edu>
2018-09-08 16:17:34 +0000

    added gif mime type and using c9 port

--- 03-http-file-demo/server.js
+++ 03-http-file-demo/server.js
@@ -6,9 +6,9 @@ Here we build on the http module by including:
  - fs - for working with the file system
 ****************************************************/
 var http = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs');
+  url = require('url'),
+  path = require('path'),
+  fs = require('fs');
 
 /***************************************************
 Based on the file extension, we'll serve the 
@@ -16,15 +16,16 @@ appropriate mime type.  This isn't a perfect way
 of doing things - but its good enough for now
 ****************************************************/
 var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css", 
-    "mp4": "video/mp4", 
-    "ogv": "video/ogv"
-  };
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "gif": "image/gif",
+  "js": "text/javascript",
+  "css": "text/css",
+  "mp4": "video/mp4",
+  "ogv": "video/ogv"
+};
 
 // As before, we create an http server
 http.createServer(function(req, res) {
@@ -39,9 +40,10 @@ http.createServer(function(req, res) {
 
   try {
     stats = fs.lstatSync(filename); // throws if path doesn't exist
-  } catch (e) {
+  }
+  catch (e) {
     console.log("\tResponse is 404, not found");
-    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.write('404 Not Found\n');
     res.end();
     return;
@@ -52,22 +54,23 @@ http.createServer(function(req, res) {
     // path exists, is a file
     var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
     console.log("\tResponse is 200, serving file");
-    res.writeHead(200, {'Content-Type': mimeType} );
+    res.writeHead(200, { 'Content-Type': mimeType });
 
     var fileStream = fs.createReadStream(filename);
     // the pipe function is quite powerful - it
     // reads from the file stream and writes to the response
     // until the source stream is emptied.
     fileStream.pipe(res);
-  } else if (stats.isDirectory()) {
+  }
+  else if (stats.isDirectory()) {
     // path exists, is a directory
     // we could see if there is an index.html at this location
     // (try this as an exercise).  For now, do nothing... return
     // not found.
     console.log("\tResponse is 404, not found (directory)");
-    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.write('404 Not Found\n');
     res.end();
-  } 
+  }
 
-}).listen(3000);
+}).listen(8080);
